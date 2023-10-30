b5838450a57a953dc718fb56db7607b74b04c3f7
Marcus Silva <marcusluis.mail@gmail.com>
2023-10-30 18:43:23 -0300

    criar usuario concluido

---
+++ appDiabemBackend/src/main/java/br/com/diabem/appDiabemBackend/AppDiabemBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.diabem.appDiabemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDiabemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDiabemBackendApplication.class, args);
+	}
+
+}
---
+++ appDiabemBackend/src/test/java/br/com/diabem/appDiabemBackend/AppDiabemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diabem.appDiabemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiabemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
